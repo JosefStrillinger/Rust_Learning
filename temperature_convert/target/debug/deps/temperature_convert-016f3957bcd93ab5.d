1fb0ca19f4f824aede42037cde6b97d0478ac64b
Josef Strillinger <jstrillinger@outlook.com>
2023-07-30 14:33:48 +0200

    v1.3

---
+++ temperature_convert/target/debug/deps/temperature_convert-016f3957bcd93ab5.d
@@ -0,0 +1,5 @@
+C:\GitHub\Rust_Learning\temperature_convert\target\debug\deps\temperature_convert-016f3957bcd93ab5.rmeta: src\main.rs
+
+C:\GitHub\Rust_Learning\temperature_convert\target\debug\deps\temperature_convert-016f3957bcd93ab5.d: src\main.rs
+
+src\main.rs:
---
+++ temperature_convert/target/debug/deps/temperature_convert-945cbc2dbf3c23c6.d
@@ -0,0 +1,5 @@
+C:\GitHub\Rust_Learning\temperature_convert\target\debug\deps\temperature_convert-945cbc2dbf3c23c6.rmeta: src\main.rs
+
+C:\GitHub\Rust_Learning\temperature_convert\target\debug\deps\temperature_convert-945cbc2dbf3c23c6.d: src\main.rs
+
+src\main.rs:
