6eb3431c2e6b3a7bb195a2ba6fd76bc5068f843c
Josef Strillinger <jstrillinger@outlook.com>
2023-11-23 23:39:08 +0100

    rust: ownership

---
+++ structs/src/main.rs
@@ -0,0 +1,38 @@
+fn main() {
+    let user1 = User {
+        active: true,
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
+
+    let user2 = User {
+        email: String::from("another@example.com"),
+        ..user1
+    };
+
+    println!("{}", user2.username);
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+    println!("{}", black.0);
+}
+
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
+}
