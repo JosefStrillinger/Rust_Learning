de3048239889fcd3d28210d898a11effb77496d1
Josef Strillinger <jstrillinger@outlook.com>
2023-11-24 23:38:30 +0100

    rust: ownership: small change

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,14 +1,14 @@
 fn main() {
-    let s1 = gives_ownership(); // gives_ownership moves its return
+    let _s1 = gives_ownership(); // gives_ownership moves its return
                                 // value into s1
 
     let s2 = String::from("hello"); // s2 comes into scope
 
-    let s3 = takes_and_gives_back(s2); // s2 is moved into
+    let _s3 = takes_and_gives_back(s2); // s2 is moved into
                                        // takes_and_gives_back, which also
                                        // moves its return value into s3
 
-    let reference_to_nothing = no_dangle();
+    let _reference_to_nothing = no_dangle();
 } // Here, s3 goes out of scope and is dropped. s2 was moved, so nothing
   // happens. s1 goes out of scope and is dropped.
 
