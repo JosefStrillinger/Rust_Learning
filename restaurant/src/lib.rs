e8f1be0a281233bcb96ea0c678151057a59e67b9
Josef Strillinger <jstrillinger@outlook.com>
2023-12-03 11:10:02 +0100

    restaurant: small update

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,23 +1,23 @@
 mod front_of_house {
     pub mod hosting {
-        pub fn add_to_waitlist() {}
+        /* pub fn add_to_waitlist() {}
 
-        fn seat_at_table() {}
+        fn seat_at_table() {} */
     }
 
     mod serving {
-        fn take_order() {}
+        /* fn take_order() {}
 
         fn serve_order() {}
 
-        fn take_payment() {}
+        fn take_payment() {} */
     }
 }
 
 mod back_of_house {
     pub struct Breakfast {
         pub toast: String,
-        seasonal_fruit: String,
+        pub seasonal_fruit: String,
     }
 
     impl Breakfast {
