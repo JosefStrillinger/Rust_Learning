36f13c33c9d89f314cb8c0a1e866935f3e3d2679
Josef Strillinger <jstrillinger@outlook.com>
2024-04-08 21:28:34 +0200

    generics: update to using generics in functions

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -10,19 +10,17 @@ fn largest<T>(list: &[T]) -> &T {
     largest
 }
 
-// struct Point<T, U> {
-//     x: T,
-//     y: U,
-// }
-
-struct Point<T> {
-    x: T,
-    y: T,
+struct Point<X1, Y1> {
+    x: X1,
+    y: Y1,
 }
 
-impl<T> Point<T> {
-    fn x(&self) -> &T {
-        &self.x
+impl<X1, Y1> Point<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
     }
 }
 
@@ -37,8 +35,10 @@ fn main() {
     let result = largest(&char_list);
     println!("The largest char is {}", result);
 
-    let both_integer = Point { x: 5, y: 10 };
-    let both_float = Point { x: 1.0, y: 4.0 };
-    // let integer_and_float = Point { x: 5, y: 4.0 };
-    let integer_and_float = Point { x: 5, y: 4 };
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
+
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }
